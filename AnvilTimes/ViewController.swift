989492c765f431734d78d3618c8d1e4e98e0aeee
Tarang Khanna <tarangalbert@gmail.com>
2015-03-07 02:35:18 -0500

    Added Eventkit, and some calendar functions

--- AnvilTimes/ViewController.swift
+++ AnvilTimes/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import EventKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let eventStore = EKEventStore()
+        
+        
+        switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent) {
+        case .Authorized:
+            insertEvent(eventStore)
+        case .Denied:
+            println("Access denied")
+        case .NotDetermined:
+         
+            eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion:
+                {[weak self] (granted: Bool, error: NSError!) -> Void in
+                    if granted {
+                        self!.insertEvent(eventStore)
+                    } else {
+                        println("Access denied")
+                    }
+            })
+        default:
+            println("Case Default")
+        }
     }
-
+    
+    func insertEvent(store: EKEventStore) {
+   
+        let calendars = store.calendarsForEntityType(EKEntityTypeEvent)
+            as [EKCalendar]
+        
+        for calendar in calendars {
+     
+            if calendar.title == "ioscreator" {
+            
+                let startDate = NSDate() //current time
+                
+                let endDate = startDate.dateByAddingTimeInterval(2 * 60 * 60)
+                
+                // Create Event
+                var event = EKEvent(eventStore: store)
+                event.calendar = calendar
+                
+                event.title = "New Meeting"
+                event.startDate = startDate
+                event.endDate = endDate
+                
+             
+                // Save Event in Calendar
+                var error: NSError?
+                let result = store.saveEvent(event, span: EKSpanThisEvent, error: &error)
+                
+                if result == false {
+                    if let theError = error {
+                        println("An error occured \(theError)")
+                    }
+                }
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
